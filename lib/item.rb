7efe98cdb1e654012c6071b191e06cdc2cc19aa9
Kirk Smith <kirks@hotmail.co.uk>
2016-07-18 20:01:40 +0100

    added catalogue class variable to Item

--- lib/item.rb
+++ lib/item.rb
@@ -1,10 +1,17 @@
 class Item
 
-  attr_reader :code, :name, :price
+  attr_reader :code, :name, :price, :catalogue
+
+  @@catalogue = []
 
   def initialize(code, name, price)
     @code = code
     @name = name
     @price = price
+    @@catalogue << self
+  end
+
+  def self.catalogue
+    @@catalogue
   end
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,17 +1,29 @@
 describe Item do
 
-  subject(:item1){described_class.new('001','Lavender heart', 9.25)}
+  let(:item1){described_class.new('001', 'Lavender heart', 9.25)}
+  let(:item2){ described_class.new('002', 'Personalised cufflinks', 45.00)}
 
-  it 'sets item code correctly' do
-    expect(item1.code).to eq('001')
+
+
+  describe 'catalogue' do
+    it 'holds all Item instances inside @@catalogue' do
+      expect(Item.catalogue).to eq([item1,item2])
+    end
   end
 
-  it 'sets item name correctly' do
-    expect(item1.name).to eq('Lavender heart')
-  end
+  describe '#initialize' do
+    it 'sets item code correctly' do
+      expect(item1.code).to eq('001')
+    end
 
-  it 'sets item price correctly' do
-    expect(item1.price).to eq(9.25)
+    it 'sets item name correctly' do
+      expect(item1.name).to eq('Lavender heart')
+    end
+
+    it 'sets item price correctly' do
+      expect(item1.price).to eq(9.25)
+    end
+
   end
 
 
