5c74ce6dd94c3b26c4e987ef8093ab45068ab302
Kirk Smith <kirks@hotmail.co.uk>
2016-07-19 21:42:11 +0100

    #apply_discounts method added to Promotion

--- lib/promotion.rb
+++ lib/promotion.rb
@@ -1,12 +1,18 @@
 class Promotion
 
+  attr_reader :target_spend_rule
+
   def initialize(target_spend_disc_klass)
     @target_spend_disc = target_spend_disc_klass
-
+    @target_spend_rule = nil
   end
 
 
   def set_target_spend_rule(target_spend, disc )
     @target_spend_rule = @target_spend_disc.new(target_spend, disc)
+  end
+
+  def apply_discounts(total)
+    @target_spend_rule.calculate_disc(total)
   end
 end
--- spec/promotion_spec.rb
+++ spec/promotion_spec.rb
@@ -2,12 +2,21 @@ describe Promotion do
 
   subject(:promo){described_class.new(target_spend_disc_klass)}
   let(:target_spend_disc_klass){double new: disc_at_60}
-  let(:disc_at_60){double calculate_disc 0}
+  let(:disc_at_60){double calculate_disc: 0}
+  let(:order) {{"001"=> 2, "002"=>1, '003'=>1}}
 
-  describe '#set_reward_discount' do
-    xit 'will set the conditions of the promotion' do
+  describe '#set_target_spend_rule' do
+    it 'will set the conditions of the promotion' do
       promo.set_target_spend_rule(60, 10)
       expect(promo.target_spend_rule).to eq(disc_at_60)
+    end
+  end
+
+
+  describe '#apply_discounts' do
+    it 'will apply target spend discount to total' do
+      allow(promo.target_spend_rule).to receive(:calculate_disc).and_return(10)
+      expect(promo.apply_discounts(100)).to eq(10)
     end
   end
 
