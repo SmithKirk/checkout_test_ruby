53839236ca29604b87bd1aa49576a20692327ded
Kirk Smith <kirks@hotmail.co.uk>
2016-07-19 23:14:01 +0100

    #calculate_disc added to MultibuyDisc

--- lib/discounts/multibuy_disc.rb
+++ lib/discounts/multibuy_disc.rb
@@ -1,10 +1,19 @@
 class MultibuyDisc
 
-  attr_reader :code, :quantity, :multibuy_price
+  attr_reader :code, :quantity, :multibuy_saving
 
-  def initialize(code, quantity, multibuy_price)
+  def initialize(code, quantity, multibuy_saving)
     @code = code
     @quantity = quantity
-    @multibuy_price = multibuy_price
+    @multibuy_saving = multibuy_saving
   end
+
+  def calculate_disc(order)
+    if order[@code] >= @quantity
+      return order[@code] * multibuy_saving
+    else
+      return 0
+    end
+  end
+
 end
--- spec/muitibuy_disc_spec.rb
+++ spec/muitibuy_disc_spec.rb
@@ -1,6 +1,9 @@
 describe MultibuyDisc do
 
-  subject(:promo){described_class.new('001', 2, 8.50)}
+  subject(:promo){described_class.new('001', 2, 0.75)}
+  let(:order1){{'001' => 2}}
+  let(:order2){{'001' => 1}}
+  let(:order3){{'001' => 1, '002' => 2}}
 
   describe '#initialize' do
     it 'will set item code for discount' do
@@ -12,7 +15,21 @@ describe MultibuyDisc do
     end
 
     it 'will set multibuy price' do
-      expect(promo.multibuy_price).to eq(8.50)
+      expect(promo.multibuy_saving).to eq(0.75)
+    end
+  end
+
+  describe '#calculate_disc' do
+    it 'will apply discount if 2 or more 001 are in order' do
+      expect(promo.calculate_disc(order1)).to eq(1.50)
+    end
+
+    it 'will not apply discount if 1 of 001 are in order' do
+      expect(promo.calculate_disc(order2)).to eq(0)
+    end
+
+    it 'will not apply discount if 2 or more 002 are in order' do
+      expect(promo.calculate_disc(order3)).to eq(0)
     end
   end
 end
