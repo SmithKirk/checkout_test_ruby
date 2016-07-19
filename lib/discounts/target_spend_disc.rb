13a4cf8a4bc01fd705e470d8745a9edf62b33d5a
Kirk Smith <kirks@hotmail.co.uk>
2016-07-19 18:20:04 +0100

    #initialize and #calculate_disc added to TargetSpendDisc

--- lib/discounts/target_spend_disc.rb
+++ lib/discounts/target_spend_disc.rb
@@ -1,8 +1,18 @@
 class TargetSpendDisc
 
-  attr_reader :target_spend
+  attr_reader :target_spend, :discount
 
   def initialize(target_spend, discount)
     @target_spend = target_spend
+    @discount = discount
   end
+
+  def calculate_disc(total)
+    if total > @target_spend
+      return total * @discount
+    else
+      return 0
+    end
+  end
+  
 end
--- spec/target_spend_disc_spec.rb
+++ spec/target_spend_disc_spec.rb
@@ -7,5 +7,19 @@ describe TargetSpendDisc do
     it 'will have target spend set' do
       expect(promotion.target_spend).to eq(60)
     end
+
+    it 'will have discount percentage set' do
+      expect(promotion.discount).to eq(0.1)
+    end
+  end
+
+  describe '#calculate_disc' do
+    it 'will apply discount to total if over target spend' do
+      expect(promotion.calculate_disc(100)).to eq(10)
+    end
+
+    it 'will not apply discount if under target spend' do
+      expect(promotion.calculate_disc(50)).to eq(0)
+    end
   end
 end
